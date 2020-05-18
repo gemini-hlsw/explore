35b57bddb125af250611a13ac29f64febdf226b8
Scala Steward <scalasteward@gemini.edu>
2020-05-18 18:01:13 -0400

    Update crystal to 0.3.1

--- project/Settings.scala
+++ project/Settings.scala
@@ -10,7 +10,7 @@ object Settings {
     val catsEffect       = "2.1.3"
     val circe            = "0.13.0"
     val clue             = "0.1.0"
-    val crystal          = "0.3.0"
+    val crystal          = "0.3.1"
     val diodeData        = "1.1.8"
     // val diodeReact      = "1.1.8"
     val discipline       = "1.0.2"
