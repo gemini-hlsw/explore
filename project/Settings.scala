6356abc45213202e7bac941420d1b98922a65a62
Scala Steward <scalasteward@gemini.edu>
2020-07-08 06:01:29 -0400

    Update react-grid-layout to 0.6.2

--- project/Settings.scala
+++ project/Settings.scala
@@ -24,7 +24,7 @@ object Settings {
     val reactAladin       = "0.1.4"
     val reactAtlasKitTree = "0.2.3"
     val reactCommon       = "0.9.4"
-    val reactGridLayout   = "0.6.1"
+    val reactGridLayout   = "0.6.2"
     val reactResizable    = "0.1.1"
     val reactSemanticUI   = "0.5.9"
     val reactSizeMe       = "0.4.4"
