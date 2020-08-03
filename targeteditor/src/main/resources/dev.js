38cb95701170146ffd1761136e38a400ac9f70c4
Raúl Piaggio <rpiaggio@gemini.edu>
2020-08-03 20:16:55 -0300

    Fix error in merge.

--- targeteditor/src/main/resources/dev.js
+++ targeteditor/src/main/resources/dev.js
@@ -2,7 +2,6 @@ import "resources/theme/semantic.less";
 import "resources/less/style.less";
 import "resources/less/components/visualization.less";
 import "resources/less/vendor/aladin.less";
-import "resources/css/vendor/aladin.css";
 import "resources/css/charts.scss";
 import "resources/css/datepicker.scss";
 
