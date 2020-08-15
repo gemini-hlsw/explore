3a280ab3e121fa3c8b60c0269c7e9f2224051c12
Scala Steward <scalasteward@gemini.edu>
2020-08-15 18:01:47 -0400

    Update react-sizeme to 0.4.7

--- project/Settings.scala
+++ project/Settings.scala
@@ -30,7 +30,7 @@ object Settings {
     val reactHighcharts   = "0.1.2"
     val reactResizable    = "0.1.3"
     val reactSemanticUI   = "0.6.2"
-    val reactSizeMe       = "0.4.6"
+    val reactSizeMe       = "0.4.7"
     val scalaJsReact      = "1.7.4"
   }
 
