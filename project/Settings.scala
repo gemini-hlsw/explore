18688e6e4f00c87e21ead002a3a391829aa524db
Scala Steward <scalasteward@gemini.edu>
2020-09-09 18:01:25 -0300

    Update log4cats-loglevel to 0.1.0

--- project/Settings.scala
+++ project/Settings.scala
@@ -15,7 +15,7 @@ object Settings {
     val disciplineMUnit   = "0.2.4"
     val geminiLocales     = "0.5.0"
     val log4Cats          = "1.1.1"
-    val log4CatsLogLevel  = "0.0.2"
+    val log4CatsLogLevel  = "0.1.0"
     val lucumaCore        = "0.4.1"
     val lucumaUI          = "0.3.2"
     val monocle           = "2.1.0"
