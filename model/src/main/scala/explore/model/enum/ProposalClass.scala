c720fb1e2b19e53c231bfd6e2d5b69315f903b77
Todd Burnside <tburnside@gemini.edu>
2020-09-22 09:03:36 -0700

    Un-nest Proposal Types

--- model/src/main/scala/explore/model/enum/ProposalClass.scala
+++ model/src/main/scala/explore/model/enum/ProposalClass.scala
@@ -8,14 +8,27 @@ import lucuma.core.util.Enumerated
 sealed abstract class ProposalClass(val label: String) extends Product with Serializable
 
 object ProposalClass {
-  case object LargeProgram   extends ProposalClass("Large Program Observing at Gemini")
-  case object FastTurnaround extends ProposalClass("Fast Turnaround Observing at Gemini")
-  case object Queue          extends ProposalClass("Queue Observing at Gemini")
-  case object Classical      extends ProposalClass("Classical Observing at Gemini")
-  case object Exchange       extends ProposalClass("Exchange Observing at Keck/Subaru")
-  case object Intensive      extends ProposalClass("Intensive Program Observing at Subaru")
-  case object Other          extends ProposalClass("Other Proposal Types")
+  case object LargeProgram       extends ProposalClass("Large Program Observing at Gemini")
+  case object FastTurnaround     extends ProposalClass("Fast Turnaround Observing at Gemini")
+  case object Queue              extends ProposalClass("Queue Observing at Gemini")
+  case object Classical          extends ProposalClass("Classical Observing at Gemini")
+  case object Exchange           extends ProposalClass("Exchange Observing at Keck/Subaru")
+  case object Intensive          extends ProposalClass("Intensive Program Observing at Subaru")
+  case object DemoScience        extends ProposalClass("Demo Science")
+  case object DirectorsTime      extends ProposalClass("Directors Time")
+  case object PoorWeather        extends ProposalClass("Poor Weather")
+  case object SystemVerification extends ProposalClass("System Verification")
 
   implicit val ProposalClassEnumerated: Enumerated[ProposalClass] =
-    Enumerated.of(LargeProgram, FastTurnaround, Queue, Classical, Exchange, Intensive, Other)
+    Enumerated.of(LargeProgram,
+                  FastTurnaround,
+                  Queue,
+                  Classical,
+                  Exchange,
+                  Intensive,
+                  DemoScience,
+                  DirectorsTime,
+                  PoorWeather,
+                  SystemVerification
+    )
 }
