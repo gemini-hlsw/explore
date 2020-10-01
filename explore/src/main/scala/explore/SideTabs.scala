149eed37728d4af9b724837ce2cdb51024da9435
Carlos Quiroz <cquiroz@gemini.edu>
2020-10-01 11:31:38 -0300

    Fix for odd shifts when hovering in FF
    Signed-off-by: Carlos Quiroz <cquiroz@gemini.edu>

--- explore/src/main/scala/explore/SideTabs.scala
+++ explore/src/main/scala/explore/SideTabs.scala
@@ -35,6 +35,7 @@ object SideTabs {
 
         def tabButton(tab: AppTab): Button =
           Button(active = tab === focus,
+                 clazz = GPPStyles.SideButton,
                  onClick = p.tabs.mod(z => z.findFocus(_ === tab).getOrElse(z)).runInCB
           )(tab.title)
 
