57c86ebceface639b2bfe80362af161849b98190
Scala Steward <scalasteward@gemini.edu>
2020-10-09 18:01:44 -0300

    Update munit, munit-scalacheck to 0.7.14

--- project/Settings.scala
+++ project/Settings.scala
@@ -21,7 +21,7 @@ object Settings {
     val lucumaUI          = "0.4.0"
     val monocle           = "2.1.0"
     val mouse             = "0.25"
-    val mUnit             = "0.7.13"
+    val mUnit             = "0.7.14"
     val reactAladin       = "0.2.1"
     val reactAtlasKitTree = "0.3.0"
     val reactCommon       = "0.10.0"
