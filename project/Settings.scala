5c108b6ce34fa33a087fcdf195d3cc25fb255b69
Scala Steward <scalasteward@gemini.edu>
2020-11-02 18:02:19 -0300

    Update discipline-munit to 1.0.0

--- project/Settings.scala
+++ project/Settings.scala
@@ -13,7 +13,7 @@ object Settings {
     val clue              = "0.3.3"
     val crystal           = "0.8.1"
     val discipline        = "1.1.0"
-    val disciplineMUnit   = "0.3.0"
+    val disciplineMUnit   = "1.0.0"
     val geminiLocales     = "0.5.0"
     val log4Cats          = "1.1.1"
     val log4CatsLogLevel  = "0.1.1"
