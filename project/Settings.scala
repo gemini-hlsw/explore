6703bc7ddeca09ba927455a02407a2998d84fa39
Scala Steward <scalasteward@gemini.edu>
2020-11-02 18:01:50 -0300

    Update cats, common to 0.11.1

--- project/Settings.scala
+++ project/Settings.scala
@@ -25,7 +25,7 @@ object Settings {
     val mUnit             = "0.7.15"
     val reactAladin       = "0.2.2"
     val reactAtlasKitTree = "0.3.0"
-    val reactCommon       = "0.11.0"
+    val reactCommon       = "0.11.1"
     val reactDatepicker   = "0.1.0"
     val reactGridLayout   = "0.9.0"
     val reactHighcharts   = "0.2.0"
