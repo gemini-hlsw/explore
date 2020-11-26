39c27577511b953d52ffc97365f0174a9ff79d57
Scala Steward <scalasteward@gemini.edu>
2020-11-26 18:01:44 -0300

    Update react-semantic-ui to 0.10.1

--- project/Settings.scala
+++ project/Settings.scala
@@ -30,7 +30,7 @@ object Settings {
     val reactGridLayout   = "0.9.1"
     val reactHighcharts   = "0.2.0"
     val reactResizable    = "0.4.1"
-    val reactSemanticUI   = "0.10.0"
+    val reactSemanticUI   = "0.10.1"
     val reactSizeMe       = "0.6.1"
     val scalaJsReact      = "1.7.7"
     val sttp              = "3.0.0-RC10"
