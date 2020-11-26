d586d511a14b427a23cecf7801b107f9129eeb8a
Scala Steward <scalasteward@gemini.edu>
2020-11-26 06:01:14 -0300

    Update cats-core to 2.3.0

--- project/Settings.scala
+++ project/Settings.scala
@@ -6,8 +6,8 @@ import sbt.librarymanagement._
 object Settings {
 
   object LibraryVersions {
-    val cats              = "2.2.0"
-    val catsEffect        = "2.2.0"
+    val cats              = "2.3.0"
+    val catsEffect        = "2.3.0"
     val chimney           = "0.6.1"
     val circe             = "0.13.0"
     val clue              = "0.3.4"
