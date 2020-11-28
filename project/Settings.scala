3f6da8e5f55e243c61061bd16c3a049e9ef2259a
Scala Steward <scalasteward@gemini.edu>
2020-11-28 06:00:58 -0300

    Update lucuma-ui to 0.10.2

--- project/Settings.scala
+++ project/Settings.scala
@@ -19,7 +19,7 @@ object Settings {
     val log4CatsLogLevel  = "0.1.1"
     val lucumaCore        = "0.7.1"
     val lucumaCatalog     = "0.3.1"
-    val lucumaUI          = "0.10.1"
+    val lucumaUI          = "0.10.2"
     val monocle           = "2.1.0"
     val mouse             = "0.25"
     val mUnit             = "0.7.19"
