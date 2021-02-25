21c6038c6959f3534a907e141f58c8b2b9aee24e
Scala Steward <scalasteward@gemini.edu>
2021-02-25 18:01:09 -0300

    Update client3:circe, client3:core to 3.1.6

--- project/Settings.scala
+++ project/Settings.scala
@@ -37,7 +37,7 @@ object Settings {
     val reactSemanticUI   = "0.10.5"
     val reactTable        = "0.0.2"
     val scalaJsReact      = "1.7.7"
-    val sttp              = "3.1.5"
+    val sttp              = "3.1.6"
   }
 
   object Libraries {
