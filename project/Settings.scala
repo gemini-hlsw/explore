fd12f6a4646c964c9188151b22cfda4553978d2c
Scala Steward <scalasteward@gemini.edu>
2021-04-04 06:01:01 -0300

    Update mouse to 1.0.2

--- project/Settings.scala
+++ project/Settings.scala
@@ -24,7 +24,7 @@ object Settings {
     val lucumaSSO         = "0.0.9"
     val lucumaBC          = "0.1.0"
     val monocle           = "2.1.0"
-    val mouse             = "1.0.0"
+    val mouse             = "1.0.2"
     val mUnit             = "0.7.23"
     val reactAladin       = "0.4.3"
     val reactAtlasKitTree = "0.4.0"
