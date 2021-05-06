76a89ea54509b533c0f9c7099625dbccc59b9167
Raúl Piaggio <rpiaggio@gemini.edu>
2021-05-06 12:20:35 -0300

    Bump JS deps.

--- common/src/main/scala/react/moon/MoonPhase.scala
+++ common/src/main/scala/react/moon/MoonPhase.scala
@@ -1,7 +1,7 @@
 // Copyright (c) 2016-2021 Association of Universities for Research in Astronomy, Inc. (AURA)
 // For license information see LICENSE or https://opensource.org/licenses/BSD-3-Clause
 
-package reactmoon
+package react.moon
 
 import japgolly.scalajs.react._
 import react.common._
