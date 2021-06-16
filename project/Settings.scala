77dd73b9c3e976426ab2738c028343fba4cae127
Scala Steward <me@scala-steward.org>
2021-06-17 00:32:36 +0200

    Update crystal to 0.15.0

--- project/Settings.scala
+++ project/Settings.scala
@@ -12,7 +12,7 @@ object Settings {
     val circe             = "0.14.1"
     val circeGolden       = "0.3.0"
     val clue              = "0.15.1"
-    val crystal           = "0.14.2"
+    val crystal           = "0.15.0"
     val discipline        = "1.1.5"
     val disciplineMUnit   = "1.0.9"
     val fs2               = "3.0.4"
