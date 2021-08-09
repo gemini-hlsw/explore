801d4270b0ca8fa7cc34c95b8eeae737c9ca8a3c
Carlos Quiroz <carlos.quiroz@noirlab.edu>
2021-08-09 15:01:07 -0400

    Update fs2
    Signed-off-by: Carlos Quiroz <carlos.quiroz@noirlab.edu>

--- model/jvm/src/main/scala/explore/modes/ModesMatrixPlatform.scala
+++ model/jvm/src/main/scala/explore/modes/ModesMatrixPlatform.scala
@@ -9,14 +9,12 @@ import fs2.data.csv._
 import fs2.io.file._
 import fs2.text
 
-import java.nio.file.Path
-
 trait ModesMatrixPlatform extends ModesMatrixDecoders {
 
   def loadMatrix[F[_]: Async](path: Path): F[List[ModeRow]] =
     Files[F]
-      .readAll(path, 1024)
-      .through(text.utf8Decode)
+      .readAll(path)
+      .through(text.utf8.decode)
       .through(decodeUsingHeaders[ModeRow]())
       .compile
       .toList
