3b7e791e7c992827bd172f09235b437817a7ceaf
Raúl Piaggio <rpiaggio@gemini.edu>
2021-10-08 00:41:59 -0300

    Headers + remove unused.

--- common/src/test/scala/explore/common/arb/ArbTargetObsQueries.scala
+++ common/src/test/scala/explore/common/arb/ArbTargetObsQueries.scala
@@ -1,68 +1,68 @@
-// Copyright (c) 2016-2021 Association of Universities for Research in Astronomy, Inc. (AURA)
-// For license information see LICENSE or https://opensource.org/licenses/BSD-3-Clause
+// // Copyright (c) 2016-2021 Association of Universities for Research in Astronomy, Inc. (AURA)
+// // For license information see LICENSE or https://opensource.org/licenses/BSD-3-Clause
 
-package explore.common.arb
+// package explore.common.arb
 
-import cats.syntax.all._
-import explore.common.TargetObsQueries._
-import explore.common.TargetObsQueriesGQL
-import explore.model.arb.ArbConstraintsSummary
-import lucuma.core.arb.ArbTime
-import lucuma.core.enum.ObsActiveStatus
-import lucuma.core.enum.ObsStatus
-import lucuma.core.model.Asterism
-import lucuma.core.model.Observation
-import lucuma.core.model.Target
-import lucuma.core.util.arb.ArbEnumerated._
-import lucuma.core.util.arb.ArbGid._
-import org.scalacheck.Arbitrary
-import org.scalacheck.Arbitrary._
-import org.scalacheck.Cogen
-import org.scalacheck.Cogen._
-import org.scalacheck.Gen
+// import cats.syntax.all._
+// import explore.common.TargetObsQueries._
+// import explore.common.TargetObsQueriesGQL
+// import explore.model.arb.ArbConstraintsSummary
+// import lucuma.core.arb.ArbTime
+// import lucuma.core.enum.ObsActiveStatus
+// import lucuma.core.enum.ObsStatus
+// import lucuma.core.model.Asterism
+// import lucuma.core.model.Observation
+// import lucuma.core.model.Target
+// import lucuma.core.util.arb.ArbEnumerated._
+// import lucuma.core.util.arb.ArbGid._
+// import org.scalacheck.Arbitrary
+// import org.scalacheck.Arbitrary._
+// import org.scalacheck.Cogen
+// import org.scalacheck.Cogen._
+// import org.scalacheck.Gen
 
-import java.time.Duration
+// import java.time.Duration
 
-trait ArbTargetObsQueries {
-  import ArbConstraintsSummary._
-  import ArbTime._
+// trait ArbTargetObsQueries {
+//   import ArbConstraintsSummary._
+//   import ArbTime._
 
-  type ConstraintSet = TargetObsQueriesGQL.TargetsObsQuery.Data.Observations.Nodes.ConstraintSet
-  val ConstraintSet = TargetObsQueriesGQL.TargetsObsQuery.Data.Observations.Nodes.ConstraintSet
+//   type ConstraintSet = TargetObsQueriesGQL.TargetsObsQuery.Data.Observations.Nodes.ConstraintSet
+//   val ConstraintSet = TargetObsQueriesGQL.TargetsObsQuery.Data.Observations.Nodes.ConstraintSet
 
-  implicit val arbObsResultPointing =
-    Arbitrary[ObsResult.Pointing] {
-      Gen.oneOf(arbitrary[Target.Id].map(ObsResult.Pointing.Target.apply),
-                arbitrary[Asterism.Id].map(ObsResult.Pointing.Asterism.apply)
-      )
-    }
+//   implicit val arbObsResultPointing =
+//     Arbitrary[ObsResult.Pointing] {
+//       Gen.oneOf(arbitrary[Target.Id].map(ObsResult.Pointing.Target.apply),
+//                 arbitrary[Asterism.Id].map(ObsResult.Pointing.Asterism.apply)
+//       )
+//     }
 
-  implicit val cogenObsResultPointing: Cogen[ObsResult.Pointing] =
-    Cogen[Either[Asterism.Id, Target.Id]]
-      .contramap {
-        case ObsResult.Pointing.Target(id)   => id.asRight
-        case ObsResult.Pointing.Asterism(id) => id.asLeft
-      }
+//   implicit val cogenObsResultPointing: Cogen[ObsResult.Pointing] =
+//     Cogen[Either[Asterism.Id, Target.Id]]
+//       .contramap {
+//         case ObsResult.Pointing.Target(id)   => id.asRight
+//         case ObsResult.Pointing.Asterism(id) => id.asLeft
+//       }
 
-  implicit val arbConstraintSet = buildConstraintsSummaryArb(ConstraintSet.apply)
+//   implicit val arbConstraintSet = buildConstraintsSummaryArb(ConstraintSet.apply)
 
-  implicit val arbObsResult =
-    Arbitrary[ObsResult] {
-      for {
-        id            <- arbitrary[Observation.Id]
-        pointing      <- arbitrary[Option[ObsResult.Pointing]]
-        constraintSet <- arbitrary[ConstraintSet]
-        status        <- arbitrary[ObsStatus]
-        activeStatus  <- arbitrary[ObsActiveStatus]
-        duration      <- arbitrary[Duration]
-      } yield ObsResult(id,
-                        pointing,
-                        constraintSet,
-                        status,
-                        activeStatus,
-                        ObsResult.PlannedTime(duration)
-      )
-    }
-}
+//   implicit val arbObsResult =
+//     Arbitrary[ObsResult] {
+//       for {
+//         id            <- arbitrary[Observation.Id]
+//         pointing      <- arbitrary[Option[ObsResult.Pointing]]
+//         constraintSet <- arbitrary[ConstraintSet]
+//         status        <- arbitrary[ObsStatus]
+//         activeStatus  <- arbitrary[ObsActiveStatus]
+//         duration      <- arbitrary[Duration]
+//       } yield ObsResult(id,
+//                         pointing,
+//                         constraintSet,
+//                         status,
+//                         activeStatus,
+//                         ObsResult.PlannedTime(duration)
+//       )
+//     }
+// }
 
-object ArbTargetObsQueries extends ArbTargetObsQueries
+// object ArbTargetObsQueries extends ArbTargetObsQueries
