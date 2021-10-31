96666ab992bb3a22dd61bdd571af596f9aa8261a
Scala Steward <me@scala-steward.org>
2021-11-01 00:20:14 +0100

    Update react-semantic-ui to 0.13.0

--- project/Settings.scala
+++ project/Settings.scala
@@ -41,7 +41,7 @@ object Settings {
     val reactHighcharts   = "0.4.1"
     val reactHotkeys      = "0.3.2"
     val reactResizable    = "0.6.0"
-    val reactSemanticUI   = "0.12.0"
+    val reactSemanticUI   = "0.13.0"
     val reactTable        = "0.7.0"
     val reactVirtuoso     = "0.2.0"
     val scalaJsReact      = "2.0.0-RC3"
