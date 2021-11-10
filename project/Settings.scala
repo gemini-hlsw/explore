54a277ed20c70dee04060599c90db20b01f27119
Scala Steward <me@scala-steward.org>
2021-11-10 20:40:02 +0100

    Update scalajs-react-table to 0.7.2

--- project/Settings.scala
+++ project/Settings.scala
@@ -42,7 +42,7 @@ object Settings {
     val reactHotkeys      = "0.3.2"
     val reactResizable    = "0.7.0"
     val reactSemanticUI   = "0.13.0"
-    val reactTable        = "0.7.1"
+    val reactTable        = "0.7.2"
     val reactVirtuoso     = "0.2.1"
     val scalaJsReact      = "2.0.0-RC5"
     val pprint            = "0.6.6"
