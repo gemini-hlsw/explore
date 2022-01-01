de1067a293fde200046c4d8eb4c684f3b0cb384b
Scala Steward <me@scala-steward.org>
2022-01-01 10:01:11 +0100

    Update lucuma-core, lucuma-core-testkit to 0.21.0

--- project/Settings.scala
+++ project/Settings.scala
@@ -23,7 +23,7 @@ object Settings {
     val log4Cats          = "2.1.1"
     val log4CatsLogLevel  = "0.3.0"
     val lucumaBC          = "0.3.2"
-    val lucumaCore        = "0.15.1"
+    val lucumaCore        = "0.21.0"
     val lucumaCatalog     = "0.7.0"
     val lucumaUI          = "0.20.0"
     val lucumaSchemas     = "0.1.8"
