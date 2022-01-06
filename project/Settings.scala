241086145fcadde81bee341b09a2994b491a813f
Scala Steward <me@scala-steward.org>
2022-01-06 22:38:29 +0100

    Update lucuma-ui to 0.23.0

--- project/Settings.scala
+++ project/Settings.scala
@@ -25,7 +25,7 @@ object Settings {
     val lucumaBC          = "0.3.2"
     val lucumaCore        = "0.15.1"
     val lucumaCatalog     = "0.7.0"
-    val lucumaUI          = "0.20.0"
+    val lucumaUI          = "0.23.0"
     val lucumaSchemas     = "0.2.0"
     val lucumaSSO         = "0.0.11"
     val monocle           = "3.1.0"
