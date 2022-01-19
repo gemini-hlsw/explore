9133bb9d1eaf8b2db96ebd3440ac1b300130727f
Scala Steward <me@scala-steward.org>
2022-01-19 01:09:55 +0100

    Update log4cats-core to 2.2.0

--- project/Settings.scala
+++ project/Settings.scala
@@ -20,7 +20,7 @@ object Settings {
     val geminiLocales     = "0.7.0"
     val http4s            = "0.23.7"
     val http4sDom         = "0.2.0"
-    val log4Cats          = "2.1.1"
+    val log4Cats          = "2.2.0"
     val log4CatsLogLevel  = "0.3.0"
     val lucumaBC          = "0.3.2"
     val lucumaCore        = "0.24.0"
