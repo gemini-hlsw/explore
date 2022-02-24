ba830b66cba68243623dd1be4268db08639d1763
Scala Steward <me@scala-steward.org>
2022-02-24 22:23:14 +0100

    Update lucuma-catalog to 0.10.1

--- project/Settings.scala
+++ project/Settings.scala
@@ -24,7 +24,7 @@ object Settings {
     val log4CatsLogLevel  = "0.3.0"
     val lucumaBC          = "0.4.0"
     val lucumaCore        = "0.24.0"
-    val lucumaCatalog     = "0.10.0"
+    val lucumaCatalog     = "0.10.1"
     val lucumaUI          = "0.25.0"
     val lucumaSchemas     = "0.12.0"
     val lucumaSSO         = "0.0.14"
