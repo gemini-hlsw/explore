d0cf7a32d9700d668922162d7ba9202c34435933
Scala Steward <me@scala-steward.org>
2022-03-16 08:15:17 +0100

    Update lucuma-schemas to 0.14.0

--- project/Settings.scala
+++ project/Settings.scala
@@ -26,7 +26,7 @@ object Settings {
     val lucumaCore        = "0.26.0"
     val lucumaCatalog     = "0.10.2"
     val lucumaUI          = "0.28.0"
-    val lucumaSchemas     = "0.13.2"
+    val lucumaSchemas     = "0.14.0"
     val lucumaSSO         = "0.1.1"
     val monocle           = "3.1.0"
     val mouse             = "1.0.10"
