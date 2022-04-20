ba23ce524d8efcc84b9af0ca5edce474c24836dc
Carlos Quiroz <carlos.quiroz@noirlab.edu>
2022-04-20 19:33:27 -0400

    Refresh the aladin view on resize

--- explore/src/main/scala/explore/targeteditor/AladinCell.scala
+++ explore/src/main/scala/explore/targeteditor/AladinCell.scala
@@ -86,8 +86,10 @@ object AladinCell extends ModelOptics {
             options.modState(_.map(_.copy(fov = newFov.x))) *>
               UserTargetPreferencesUpsert
                 .updateFov[IO](props.uid, props.tid, newFov.x)
+                .whenA(fov.forall(_.x != newFov.x))
                 .runAsync
-                .debounce(1.seconds)
+                .rateLimit(1.seconds, 1)
+                .void
           }
 
         <.div(
