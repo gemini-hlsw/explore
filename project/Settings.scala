1cc3f21281cfb159df013f4d477c8f3cce029608
Scala Steward <me@scala-steward.org>
2022-04-27 06:21:13 +0200

    Update lucuma-sso-frontend-client to 0.1.4

--- project/Settings.scala
+++ project/Settings.scala
@@ -27,7 +27,7 @@ object Settings {
     val lucumaCatalog       = "0.13.0"
     val lucumaUI            = "0.34.1"
     val lucumaSchemas       = "0.17.0"
-    val lucumaSSO           = "0.1.3"
+    val lucumaSSO           = "0.1.4"
     val monocle             = "3.1.0"
     val mouse               = "1.0.10"
     val mUnit               = "0.7.29"
