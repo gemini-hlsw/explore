72fef8dda2ac491c3d4d771f89593935338f3262
Scala Steward <me@scala-steward.org>
2022-05-05 06:15:17 +0200

    Update log4cats-loglevel to 0.3.1

--- project/Settings.scala
+++ project/Settings.scala
@@ -21,7 +21,7 @@ object Settings {
     val http4s              = "0.23.11"
     val http4sDom           = "0.2.1"
     val log4Cats            = "2.3.1"
-    val log4CatsLogLevel    = "0.3.0"
+    val log4CatsLogLevel    = "0.3.1"
     val lucumaBC            = "0.4.0"
     val lucumaCore          = "0.34.1"
     val lucumaCatalog       = "0.14.2"
