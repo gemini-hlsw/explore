ddbe78a7ad09f95d63193b7ed146de4731d4c039
Raúl Piaggio <rpiaggio@gemini.edu>
2022-07-28 18:35:43 -0300

    Reinstate Undo tests.

--- common/src/test/scala/explore/undo/package.scala
+++ common/src/test/scala/explore/undo/package.scala
@@ -3,39 +3,40 @@
 
 package explore
 
-// import cats.Applicative
-// import crystal.ViewF
-// import japgolly.scalajs.react.util.DefaultEffects.{Sync => DefaultS}
+import cats.Applicative
+import crystal.ViewF
+import japgolly.scalajs.react.ReactCats._
+import japgolly.scalajs.react.util.DefaultEffects.{Sync => DefaultS}
 
-package object undo {
-//   type View[A] = ViewF[DefaultS, A]
-//
-//   object View {
-//     @inline
-//     def apply[A](
-//       value: A,
-//       modCB: (A => A, A => DefaultS[Unit]) => DefaultS[Unit]
-//     ): ViewF[DefaultS, A] = ViewF[DefaultS, A](value, modCB)
-//   }
-// }
-//
-// package undo {
-//   class VarRef[F[_]: Applicative, A](init: A) {
-//     var a: A = init
-//
-//     def get: F[A] = Applicative[F].pure(a)
-//
-//     def update(f: A => A): F[A] = {
-//       a = f(a)
-//       get
-//     }
-//   }
-//
-//   object VarRef {
-//     def apply[F[_]: Applicative]: Applied[F] = new Applied[F]
-//
-//     class Applied[F[_]: Applicative] {
-//       def of[A](init: A): VarRef[F, A] = new VarRef(init)
-//     }
-//   }
+package object undo                           {
+  type View[A] = ViewF[DefaultS, A]
+
+  object View {
+    @inline
+    def apply[A](
+      value: A,
+      modCB: (A => A, A => DefaultS[Unit]) => DefaultS[Unit]
+    ): ViewF[DefaultS, A] = ViewF[DefaultS, A](value, modCB)
+  }
+}
+
+package undo {
+  class VarRef[F[_]: Applicative, A](init: A) {
+    var a: A = init
+
+    def get: F[A] = Applicative[F].pure(a)
+
+    def update(f: A => A): F[A] = {
+      a = f(a)
+      get
+    }
+  }
+
+  object VarRef {
+    def apply[F[_]: Applicative]: Applied[F] = new Applied[F]
+
+    class Applied[F[_]: Applicative] {
+      def of[A](init: A): VarRef[F, A] = new VarRef(init)
+    }
+  }
 }
