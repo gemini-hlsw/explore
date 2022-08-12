dbd8bc8786eda887df0ea917d0b3bfd3f28faf37
Carlos Quiroz <carlos.quiroz@noirlab.edu>
2022-08-12 15:34:54 -0400

    Don't use Enumerated.of

--- model/shared/src/main/scala/explore/model/enum/ItcSeriesDataType.scala
+++ model/shared/src/main/scala/explore/model/enum/ItcSeriesDataType.scala
@@ -3,18 +3,26 @@
 
 package explore.model.enums
 
+import lucuma.core.syntax.string._
 import lucuma.core.util.Enumerated
 
-enum ItcSeriesDataType:
-  case SignalData, BackgroundData, SingleS2NData, FinalS2NData, PixSigData, PixBackData
+enum ItcSeriesDataType(val tag: String):
+  case SignalData     extends ItcSeriesDataType("signal_data")
+  case BackgroundData extends ItcSeriesDataType("background_data")
+  case SingleS2NData  extends ItcSeriesDataType("single_s2_ndata")
+  case FinalS2NData   extends ItcSeriesDataType("final_s2_ndata")
+  case PixSigData     extends ItcSeriesDataType("pix_sig_data")
+  case PixBackData    extends ItcSeriesDataType("pick_back_data")
 
 object ItcSeriesDataType:
   given Enumerated[ItcSeriesDataType] =
-    Enumerated.of(
-      ItcSeriesDataType.SignalData,
-      ItcSeriesDataType.BackgroundData,
-      ItcSeriesDataType.SingleS2NData,
-      ItcSeriesDataType.FinalS2NData,
-      ItcSeriesDataType.PixSigData,
-      ItcSeriesDataType.PixBackData
-    )
+    Enumerated
+      .from(
+        ItcSeriesDataType.SignalData,
+        ItcSeriesDataType.BackgroundData,
+        ItcSeriesDataType.SingleS2NData,
+        ItcSeriesDataType.FinalS2NData,
+        ItcSeriesDataType.PixSigData,
+        ItcSeriesDataType.PixBackData
+      )
+      .withTag(_.tag)
