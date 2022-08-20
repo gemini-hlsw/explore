a1a1eb81ba4c9371bc1c9e6322996f86e6181121
Carlos Quiroz <carlos.quiroz@noirlab.edu>
2022-08-20 16:06:56 -0400

    Port the rest of the enums

--- model/shared/src/main/scala/explore/model/enum/ImagingCapabilities.scala
+++ model/shared/src/main/scala/explore/model/enum/ImagingCapabilities.scala
@@ -5,14 +5,12 @@ package explore.model.enums
 
 import lucuma.core.util.Enumerated
 
-sealed abstract class ImagingCapabilities(val label: String) extends Product with Serializable
+enum ImagingCapabilities(val label: String):
+  case Polarimetry        extends ImagingCapabilities("Polarimetry")
+  case Speckle            extends ImagingCapabilities("Speckle")
+  case Corongraphy        extends ImagingCapabilities("Corongraphy")
+  case LowOverheadReadout extends ImagingCapabilities("Low Overhead Readout")
 
-object ImagingCapabilities {
-  case object Polarimetry        extends ImagingCapabilities("Polarimetry")
-  case object Speckle            extends ImagingCapabilities("Speckle")
-  case object Corongraphy        extends ImagingCapabilities("Corongraphy")
-  case object LowOverheadReadout extends ImagingCapabilities("Low Overhead Readout")
-
-  implicit val ConfigurationModeEnumerated: Enumerated[ImagingCapabilities] =
-    Enumerated.of(Polarimetry, Speckle, Corongraphy, LowOverheadReadout)
-}
+object ImagingCapabilities:
+  given Enumerated[ImagingCapabilities] =
+    Enumerated.from(Polarimetry, Speckle, Corongraphy, LowOverheadReadout).withTag(_.label)
