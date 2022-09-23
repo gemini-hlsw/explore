07b2a45de5509c5323ed0da1b921cdbd216ce029
Carlos Quiroz <carlos.quiroz@noirlab.edu>
2022-09-23 09:39:47 -0300

    Better responsivness for the obs time editor

--- explore/src/main/scala/explore/config/VizTimeEditor.scala
+++ explore/src/main/scala/explore/config/VizTimeEditor.scala
@@ -3,64 +3,43 @@
 
 package explore.config
 
-import cats.syntax.all._
+import cats.syntax.all.*
 import crystal.Pot
 import crystal.react.View
-import eu.timepit.refined.auto._
+import eu.timepit.refined.auto.*
 import explore.components.HelpIcon
 import explore.components.ui.ExploreStyles
-import explore.utils._
-import japgolly.scalajs.react._
-import japgolly.scalajs.react.vdom.html_<^._
+import explore.utils.*
+import japgolly.scalajs.react.*
+import japgolly.scalajs.react.vdom.html_<^.*
 import lucuma.refined.*
 import lucuma.ui.syntax.all.*
 import lucuma.ui.syntax.all.given
 import react.common.ReactFnProps
-import react.datepicker._
-import react.semanticui.collections.form.Form
-import react.semanticui.sizes._
+import react.datepicker.*
+import react.semanticui.sizes.*
 
 import java.time.Instant
 
 import scalajs.js
 import scalajs.js.|
 
-final case class VizTimeEditor(vizTimeView: Pot[View[Option[Instant]]])
-    extends ReactFnProps[VizTimeEditor](VizTimeEditor.component)
+case class VizTimeEditor(vizTimeView: Pot[View[Option[Instant]]])
+    extends ReactFnProps(VizTimeEditor.component)
 
 object VizTimeEditor {
-  type Props = VizTimeEditor
+  private type Props = VizTimeEditor
 
   // TODO Move these to react-datetime
-  implicit class InstantOps(val instant: Instant) extends AnyVal {
+  extension (instant: Instant)
     // DatePicker only works in local timezone, so we trick it by adding the timezone offset.
     // See https://github.com/Hacker0x01/react-datepicker/issues/1787
     def toDatePickerJsDate: js.Date =
       new js.Date(instant.toEpochMilli.toDouble + (new js.Date()).getTimezoneOffset() * 60000)
-  }
 
-  object InstantBuilder {
-    // DatePicker only works in local timezone, so we trick it by adding the timezone offset.
-    // See https://github.com/Hacker0x01/react-datepicker/issues/1787
-    def fromDatePickerJsDate(jsDate: js.Date): Instant =
-      Instant.ofEpochMilli((jsDate.getTime() - jsDate.getTimezoneOffset() * 60000).toLong)
-  }
-
-  implicit class JSUndefOrNullOrTuple2DateTimeOps[A](
-    val value: js.UndefOr[DateOrRange]
-  ) extends AnyVal {
-    def toEitherOpt2: Option[Either[(A, A), A]] =
-      value.toOption
-        .flatMap(valueOrNull => Option(valueOrNull.asInstanceOf[A | js.Tuple2[A, A]]))
-        .map { valueOrTuple =>
-          if (js.Array.isArray(valueOrTuple))
-            Left(valueOrTuple.asInstanceOf[js.Tuple2[A, A]])
-          else
-            Right(valueOrTuple.asInstanceOf[A])
-        }
-
-    def fromDatePickerToInstantEitherOpt(implicit
-      ev: A <:< js.Date
+  extension [A](value: js.UndefOr[DateOrRange])
+    def fromDatePickerToInstantEitherOpt(using
+      A <:< js.Date
     ): Option[Either[(Instant, Instant), Instant]] =
       value.toEitherOpt.map { (e: Either[(js.Date, js.Date), js.Date]) =>
         e match {
@@ -71,35 +50,40 @@ object VizTimeEditor {
         }
       }.widen
 
-    def fromDatePickerToInstantOpt(implicit ev: A <:< js.Date): Option[Instant] =
+    def fromDatePickerToInstantOpt(using ev: A <:< js.Date): Option[Instant] =
       fromDatePickerToInstantEitherOpt.flatMap(_.toOption)
+
+  object InstantBuilder {
+    // DatePicker only works in local timezone, so we trick it by adding the timezone offset.
+    // See https://github.com/Hacker0x01/react-datepicker/issues/1787
+    def fromDatePickerJsDate(jsDate: js.Date): Instant =
+      Instant.ofEpochMilli((jsDate.getTime() - jsDate.getTimezoneOffset() * 60000).toLong)
   }
 
-  val component =
+  private val component =
     ScalaFnComponent[Props] { p =>
       <.div(
-        ExploreStyles.ObsConfigurationObsTime,
-        Form(size = Small)(
-          ExploreStyles.Compact,
-          ExploreStyles.ObsInstantTileTitle,
-          potRender[View[Option[Instant]]](
-            pendingRender = EmptyVdom,
-            valueRender = instant =>
-              React.Fragment(
-                <.label("Observation time", HelpIcon("configuration/obstime.md".refined)),
-                Datepicker(onChange =
-                  (newValue, _) =>
-                    newValue.fromDatePickerToInstantOpt.foldMap { i =>
-                      instant.set(i.some)
-                    }
-                )
-                  .showTimeInput(true)
-                  .selected(instant.get.getOrElse(Instant.now).toDatePickerJsDate)
-                  .dateFormat("yyyy-MM-dd HH:mm"),
-                "UTC"
+        ExploreStyles.ObsInstantTileTitle,
+        potRender[View[Option[Instant]]](
+          pendingRender = EmptyVdom,
+          valueRender = instant =>
+            React.Fragment(
+              <.label(dataAbbrv := "Time",
+                      <.span("Observation time"),
+                      HelpIcon("configuration/obstime.md".refined)
+              ),
+              Datepicker(onChange =
+                (newValue, _) =>
+                  newValue.fromDatePickerToInstantOpt.foldMap { i =>
+                    instant.set(i.some)
+                  }
               )
-          )(p.vizTimeView)
-        )
+                .showTimeInput(true)
+                .selected(instant.get.getOrElse(Instant.now).toDatePickerJsDate)
+                .dateFormat("yyyy-MM-dd HH:mm"),
+              <.label("UTC")
+            )
+        )(p.vizTimeView)
       )
     }
 
--- explore/src/main/webapp/main.jsx
+++ explore/src/main/webapp/main.jsx
@@ -5,6 +5,7 @@ import "/common/sass/tooltips.scss";
 import "/common/less/vendor/aladin.less";
 import "/common/sass/charts.scss";
 import "/common/sass/datepicker.scss";
+import "/common/sass/explore.scss";
 import "/common/less/vendor/react-reflex.less";
 import "github-markdown-css/github-markdown-light.css";
 import "react-circular-progressbar/dist/styles.css";
