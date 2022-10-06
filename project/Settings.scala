434a65dfd7aec799d3057a82d346f39a9eca0dda
Arman Bilge <armanbilge@gmail.com>
2022-10-06 16:07:20 +0000

    bump clue

--- project/Settings.scala
+++ project/Settings.scala
@@ -12,7 +12,7 @@ object Settings {
     val catsRetry             = "3.1.0"
     val circe                 = "0.14.3"
     val circeGolden           = "0.3.0"
-    val clue                  = "0.24-d573768-SNAPSHOT"
+    val clue                  = "0.24-6300e0b-SNAPSHOT"
     val crystal               = "0.31.6"
     val discipline            = "1.5.1"
     val disciplineMUnit       = "1.0.9"
