e3a6ce7d66df34ef5fa4a60dc2c24e53d0869ce5
Carlos Quiroz <carlos.quiroz@noirlab.edu>
2022-11-03 09:23:25 -0300

    Sort by unit and value

--- explore/src/main/scala/explore/targets/TargetColumns.scala
+++ explore/src/main/scala/explore/targets/TargetColumns.scala
@@ -3,6 +3,7 @@
 
 package explore.targets
 
+import cats.Order
 import cats.Order.*
 import cats.syntax.all.*
 import eu.timepit.refined.types.string.NonEmptyString
@@ -132,6 +133,9 @@ object TargetColumns:
         <.span(ExploreStyles.UnitsTableLabel, measure.units.shortName.replace(" mag", ""))
       )
 
+    // Order first by unit alphabetically and then value
+    private given Order[Measure[BigDecimal]] = Order.by(x => (x.units.abbv, x.value))
+
     val siderealColumns =
       List(
         siderealColumn(RAColumnId, Target.Sidereal.baseRA.get)
@@ -150,7 +154,7 @@ object TargetColumns:
           ).setCell(_.value.map(displayWithoutError(_)(displayBrightness)).orEmpty)
             .setSize(80.toPx)
             // By user request we allow sorting by value though there maybe a mix of units
-            .sortableBy(_.map(_.value))
+            .sortable
         ) ++
         List(
           siderealColumn(EpochColumnId, Target.Sidereal.epoch.get)
