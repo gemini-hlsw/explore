6ae858a51e614bf15ef07f1da1c980470f58f5eb
Todd Burnside <tburnside@gemini.edu>
2023-02-16 13:48:17 -0300

    Fix problem of ITC not being called for spectroscopy modes table.

--- explore/src/main/scala/explore/config/SpectroscopyModesTable.scala
+++ explore/src/main/scala/explore/config/SpectroscopyModesTable.scala
@@ -505,21 +505,23 @@ private object SpectroscopyModesTable extends TableHooks:
       // atTop
       .useState(false)
       // Recalculate ITC values if the wv or sn change or if the rows get modified
-      .useStreamResourceBy((props, _, _, _, _, _, _, _, _, visibleRows, _) =>
+      .useStreamResourceBy((props, _, _, rows, _, _, _, _, _, _, _) =>
         (
           props.spectroscopyRequirements.wavelength,
           props.spectroscopyRequirements.signalToNoise,
           props.spectroscopyRequirements.signalToNoiseAt,
           props.constraints,
-          props.brightestTarget
+          props.brightestTarget,
+          rows.length
         )
       ) {
-        (_, ctx, itcResults, _, itcProgress, _, _, _, table, visibleRows, _) => (
+        (_, ctx, itcResults, _, itcProgress, _, _, _, table, _, _) => (
           wavelength,
           signalToNoise,
           signalToNoiseAt,
           constraints,
-          brightestTarget
+          brightestTarget,
+          _
         ) =>
           import ctx.given
 
@@ -555,6 +557,7 @@ private object SpectroscopyModesTable extends TableHooks:
                 (for
                   _       <- Resource.eval(itcProgress.setStateAsync(progressZero))
                   request <-
+                    println("Calling ItcClient.request(ItcMessage.Query)")
                     ItcClient[IO]
                       .request(
                         ItcMessage.Query(w, sn, constraints, t, modes.map(_.entry), signalToNoiseAt)
