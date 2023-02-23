27277d36ece6af08981377c4d2ba508ce2dbcfb7
Todd Burnside <tburnside@gemini.edu>
2023-02-23 15:04:37 -0300

    Handle errors from accessing localStorage

--- common/src/main/scala/explore/model/ExploreClipboard.scala
+++ common/src/main/scala/explore/model/ExploreClipboard.scala
@@ -6,11 +6,12 @@ package explore.model
 import cats.effect.IO
 import explore.model.LocalClipboard.*
 import org.scalajs.dom
+import org.typelevel.log4cats.Logger
 
 object ExploreClipboard:
   private val storageKey = "clipboard"
 
-  def get: IO[LocalClipboard] = IO {
+  def get(using Logger[IO]): IO[LocalClipboard] = IO {
     // getItem returns null when storage isn't set.
     Option(dom.window.localStorage.getItem(storageKey)).fold(LocalClipboard.Empty)(value =>
       ObsIdSet.fromString
@@ -19,9 +20,11 @@ object ExploreClipboard:
         .orElse(TargetIdSet.fromString.getOption(value).map(LocalClipboard.CopiedTargets.apply))
         .getOrElse(LocalClipboard.Empty)
     )
-  }
+  }.handleErrorWith(t =>
+    Logger[IO].error(t)("Error getting value from localStorage") >> IO(LocalClipboard.Empty)
+  )
 
-  def set(item: LocalClipboard): IO[Unit] = IO {
+  def set(item: LocalClipboard)(using Logger[IO]): IO[Unit] = IO {
     item match {
       case CopiedObservations(oids) =>
         dom.window.localStorage.setItem(storageKey, ObsIdSet.fromString.reverseGet(oids))
@@ -29,4 +32,4 @@ object ExploreClipboard:
         dom.window.localStorage.setItem(storageKey, TargetIdSet.fromString.reverseGet(tids))
       case Empty                    => dom.window.localStorage.removeItem(storageKey)
     }
-  }
+  }.handleErrorWith(t => Logger[IO].error(t)("Error setting localStorage"))
