13f6dfc1f07815998ff2fdc21145e464f3eed805
Raúl Piaggio <rpiaggio@gemini.edu>
2023-03-15 11:51:09 -0300

    reenable-and-or-demo

--- observationtree/src/main/scala/explore/observationtree/Test.scala
+++ observationtree/src/main/scala/explore/observationtree/Test.scala
@@ -5,29 +5,29 @@ package explore.observationtree
 
 import explore.AppMain
 import explore._
-import explore.components.state.IfLogged
+// import explore.components.state.IfLogged
 import explore.model.RootModel
 import japgolly.scalajs.react.vdom.html_<^._
 
 import scala.scalajs.js.annotation.JSExportTopLevel
 
-import TargetObsQueries._
+// import TargetObsQueries._
 
 @JSExportTopLevel("ObsTreeTest")
 object Test extends AppMain {
 
   override def rootComponent(view: View[RootModel]): VdomElement =
-    IfLogged(view)((_, _) =>
-      // AndOrTest.render
-      // TargetTree(TargetTreeTest.targets, TargetTreeTest.observations)
-      TargetObsLiveQuery(targetsWithObs =>
-        <.div(^.width := "295px")(
-          TargetObsList(
-            targetsWithObs,
-            view.zoom(RootModel.focused),
-            view.zoom(RootModel.targetViewExpandedIds)
-          )
-        )
-      )
-    )
+    AndOrTest.render
+  // IfLogged(view)((_, _) =>
+  // TargetTree(TargetTreeTest.targets, TargetTreeTest.observations)
+  // TargetObsLiveQuery(targetsWithObs =>
+  //   <.div(^.width := "295px")(
+  //     TargetObsList(
+  //       targetsWithObs,
+  //       view.zoom(RootModel.focused),
+  //       view.zoom(RootModel.targetViewExpandedIds)
+  //     )
+  //   )
+  // )
+  // )
 }
