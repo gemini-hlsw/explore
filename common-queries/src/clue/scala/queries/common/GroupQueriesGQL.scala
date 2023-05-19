c86ba8c06154fe75b1d1efcff8e8e5dda3ef9894
Carlos Quiroz <carlos.quiroz@noirlab.edu>
2023-05-19 10:25:56 -0400

    Update typelevel libs

--- common-queries/src/clue/scala/queries/common/GroupQueriesGQL.scala
+++ common-queries/src/clue/scala/queries/common/GroupQueriesGQL.scala
@@ -1,3 +1,6 @@
+// Copyright (c) 2016-2023 Association of Universities for Research in Astronomy, Inc. (AURA)
+// For license information see LICENSE or https://opensource.org/licenses/BSD-3-Clause
+
 package queries.common
 
 import clue.GraphQLSubquery
