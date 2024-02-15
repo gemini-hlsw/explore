918d6d349a659944f16bc9da7c0ff1c3abbeac67
Todd Burnside <tburnside@gemini.edu>
2024-02-15 10:14:18 -0300

    Address review comments.

--- common/src/main/scala/explore/cache/CacheComponent.scala
+++ common/src/main/scala/explore/cache/CacheComponent.scala
@@ -16,8 +16,19 @@ import japgolly.scalajs.react.vdom.html_<^.*
 trait CacheComponent[S, P <: CacheComponent.Props[S]: Reusability]:
   private type F[T] = DefaultA[T]
 
+  // Initializes the cache.
+  // `P` is the Properties
+  // `(S => S) => F[Unit]` is a delayed update function. Things fed to this function
+  // will be merged with the main update stream. The idea is that this will be done
+  // asynchronously. BUT, it is the responsibility of the implementation to
+  // call `.start` on the IO returned by this function. This gives the implementation
+  // more control over when the IO will be started.
   protected val initial: (P, (S => S) => F[Unit]) => F[S]
 
+  // A stream of updates to the cache.
+  // See explanation of types above.
+  // Plus, in the output, the stream values must be `F[S => S]` in order to support
+  // the asnchronous updates.
   protected val updateStream: (P, (S => S) => F[Unit]) => Resource[F, fs2.Stream[F, F[S => S]]]
 
   val component =
@@ -27,15 +38,15 @@ trait CacheComponent[S, P <: CacheComponent.Props[S]: Reusability]:
         props =>
           for
             latch        <- Deferred[F, SignallingRef[F, S]]
-            effects      <- SignallingRef[F].of(identity[S])
+            delayUpdate  <- SignallingRef[F].of(identity[S])
             // Start the update fiber. We want subscriptions to start before initial query.
             // This way we don't miss updates.
             // The update fiber Will only update the cache once it is initialized (via latch).
             // TODO: RESTART CACHE IN CASE OF INTERRUPTED SUBSCRIPTION.
             _            <-
-              updateStream(props, effects.set)
+              updateStream(props, delayUpdate.set)
                 .map(
-                  _.merge(effects.discrete.map(Async[F].pure))
+                  _.merge(delayUpdate.discrete.map(Async[F].pure))
                 )
                 .evalTap(
                   _.evalTap(fmod =>
@@ -44,7 +55,7 @@ trait CacheComponent[S, P <: CacheComponent.Props[S]: Reusability]:
                 )
                 .useForever
                 .start
-            initialValue <- initial(props, effects.set)
+            initialValue <- initial(props, delayUpdate.set)
             cache        <- SignallingRef[F].of(initialValue)
             _            <- latch.complete(cache) // Allow stream updates to proceed.
           yield cache
@@ -52,8 +63,7 @@ trait CacheComponent[S, P <: CacheComponent.Props[S]: Reusability]:
       .useStreamBy((props, cache) => (props, cache.isReady))((props, cache) =>
         _ =>
           cache.toOption.map(_.discrete).orEmpty.evalTap { value =>
-            cats.effect.Async[F].delay(println("Emitting a value")) >>
-              props.setState(value.some)
+            props.setState(value.some)
           }
       )
       // .useEffectWithDepsBy((_, _, value) => value.toOption)((props, _, _) =>
