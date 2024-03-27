d1f26bace71aa974491f8368a5bb7713df462084
Hugo van Rijswijk <git@hugovr.nl>
2024-03-27 12:25:29 +0100

    Add key prop to group edit tile
    This fixes the issue the 'type' button selection not switching

--- explore/src/main/scala/explore/tabs/ObsGroupTiles.scala
+++ explore/src/main/scala/explore/tabs/ObsGroupTiles.scala
@@ -66,7 +66,11 @@ object ObsGroupTiles:
         s"${if group.get.isAnd then "AND" else "OR"} Group",
         props.backButton.some,
         tileTitleClass = ExploreStyles.GroupEditTitle
-      )(GroupEditTile(group, node.get._1.children.length, props.timeEstimateRange, _))
+      )(
+        GroupEditTile(group, node.get._1.children.length, props.timeEstimateRange, _)
+          .withKey(props.groupId.toString)
+          .toUnmounted
+      )
 
       val notesTile = Tile(
         GroupEditIds.GroupNotesId.id,
