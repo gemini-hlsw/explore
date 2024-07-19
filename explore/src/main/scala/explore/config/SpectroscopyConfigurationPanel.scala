e5f5b080307dc540ec2ff9ad94ebce88757cb99d
Carlos Quiroz <carlos.quiroz@noirlab.edu>
2024-07-19 17:01:43 -0400

    Deactivate unavailable options on the spectroscopy configuration form

--- explore/src/main/scala/explore/config/SpectroscopyConfigurationPanel.scala
+++ explore/src/main/scala/explore/config/SpectroscopyConfigurationPanel.scala
@@ -5,6 +5,7 @@ package explore.config
 
 import cats.syntax.all.*
 import crystal.react.View
+import crystal.react.hooks.*
 import eu.timepit.refined.cats.*
 import eu.timepit.refined.types.string.NonEmptyString
 import explore.components.HelpIcon
@@ -16,6 +17,7 @@ import explore.model.display.given
 import japgolly.scalajs.react.*
 import japgolly.scalajs.react.feature.ReactFragment
 import japgolly.scalajs.react.vdom.html_<^.*
+import lucuma.core.enums.FocalPlane
 import lucuma.core.math.SignalToNoise
 import lucuma.core.math.Wavelength
 import lucuma.core.validation.*
@@ -49,133 +51,137 @@ object SpectroscopyConfigurationPanel extends ConfigurationFormats {
   type Props = SpectroscopyConfigurationPanel
 
   protected val component =
-    ScalaFnComponent[Props] { p =>
-      val prevSignalToNoiseAt = p.options.get.signalToNoiseAt
+    ScalaFnComponent
+      .withHooks[Props]
+      .useStateView(
+        FocalPlane.SingleSlit.some.widen[FocalPlane]
+      ) // For now only SlitView is allowed
+      .render { (p, fpView) =>
+        val prevSignalToNoiseAt = p.options.get.signalToNoiseAt
 
-      // Set SignalToNoiseAt to wavelength if it is empty
-      val options = p.options.withOnMod(s =>
-        if (s.wavelength =!= prevSignalToNoiseAt && prevSignalToNoiseAt.isEmpty)
-          p.options.set(s.copy(signalToNoiseAt = s.wavelength))
-        else Callback.empty
-      )
+        // Set SignalToNoiseAt to wavelength if it is empty
+        val options = p.options.withOnMod(s =>
+          if (s.wavelength =!= prevSignalToNoiseAt && prevSignalToNoiseAt.isEmpty)
+            p.options.set(s.copy(signalToNoiseAt = s.wavelength))
+          else Callback.empty
+        )
 
-      val resolution             = options.zoom(ScienceRequirements.Spectroscopy.resolution)
-      val signalToNoise          = options.zoom(ScienceRequirements.Spectroscopy.signalToNoise)
-      val signalToNoiseAt        = options.zoom(ScienceRequirements.Spectroscopy.signalToNoiseAt)
-      val wv                     = options.zoom(ScienceRequirements.Spectroscopy.wavelength)
-      val wavelengthDelta        = options.zoom(ScienceRequirements.Spectroscopy.wavelengthCoverage)
-      val focalPlane             = options.zoom(ScienceRequirements.Spectroscopy.focalPlane)
-      val focalPlaneAngle        = options.zoom(ScienceRequirements.Spectroscopy.focalPlaneAngle)
-      val spectroscopyCapability =
-        options.zoom(ScienceRequirements.Spectroscopy.capability)
+        val resolution             = options.zoom(ScienceRequirements.Spectroscopy.resolution)
+        val signalToNoise          = options.zoom(ScienceRequirements.Spectroscopy.signalToNoise)
+        val signalToNoiseAt        = options.zoom(ScienceRequirements.Spectroscopy.signalToNoiseAt)
+        val wv                     = options.zoom(ScienceRequirements.Spectroscopy.wavelength)
+        val wavelengthDelta        = options.zoom(ScienceRequirements.Spectroscopy.wavelengthCoverage)
+        val focalPlaneAngle        = options.zoom(ScienceRequirements.Spectroscopy.focalPlaneAngle)
+        val spectroscopyCapability =
+          options.zoom(ScienceRequirements.Spectroscopy.capability)
 
-      val wvMicroInput        = ExploreModelValidators.wavelengthValidWedge.optional
-      val wvcMicroInput       = ExploreModelValidators.wavelengthDeltaValidWedge.optional
-      val wvBaseAuditor       = ChangeAuditor
-        .fromInputValidWedge(ExploreModelValidators.wavelengthValidWedge)
-        .allow(s => s === "0" || s === "0.")
-      val wvChangeAuditor     = wvBaseAuditor
-        .decimal(3.refined)
-        .optional
-      val snAtWvChangeAuditor = wvBaseAuditor
-        .decimal(4.refined)
-        .optional
+        val wvMicroInput        = ExploreModelValidators.wavelengthValidWedge.optional
+        val wvcMicroInput       = ExploreModelValidators.wavelengthDeltaValidWedge.optional
+        val wvBaseAuditor       = ChangeAuditor
+          .fromInputValidWedge(ExploreModelValidators.wavelengthValidWedge)
+          .allow(s => s === "0" || s === "0.")
+        val wvChangeAuditor     = wvBaseAuditor
+          .decimal(3.refined)
+          .optional
+        val snAtWvChangeAuditor = wvBaseAuditor
+          .decimal(4.refined)
+          .optional
 
-      ReactFragment(
-        FormInputTextView[View, Option[Wavelength]](
-          id = "configuration-wavelength".refined,
-          value = wv,
-          label = ReactFragment(
-            "Wavelength",
-            HelpIcon("configuration/wavelength.md".refined)
-          ),
-          groupClass = ExploreStyles.WarningInput.when_(wv.get.isEmpty),
-          postAddons = wv.get.fold(List(requiredForITC))(_ => Nil),
-          units = "μm",
-          validFormat = wvMicroInput,
-          changeAuditor = wvChangeAuditor,
-          disabled = p.readonly
-        ).clearable(^.autoComplete.off),
-        FormInputTextView(
-          id = "configuration-resolution-power".refined,
-          value = resolution,
-          label = ReactFragment(
-            "λ / Δλ",
-            HelpIcon("configuration/spectral_resolution.md".refined)
-          ),
-          validFormat = InputValidSplitEpi.posInt.optional,
-          changeAuditor = ChangeAuditor.posInt.optional,
-          disabled = p.readonly
-        ).clearable(^.autoComplete.off),
-        FormLabel("signal-to-noise".refined)(
-          "S / N",
-          HelpIcon("configuration/signal_to_noise.md".refined)
-        ),
-        <.div(
-          LucumaPrimeStyles.FormField |+| ExploreStyles.BasicConfigurationSNAt,
-          FormInputTextView(
-            id = "signal-to-noise".refined,
-            value = signalToNoise,
-            groupClass = ExploreStyles.WarningInput.when_(signalToNoise.get.isEmpty),
-            validFormat = ExploreModelValidators.signalToNoiseValidSplitEpi.optional,
-            postAddons = signalToNoise.get.fold(List(requiredForITC))(_ => Nil),
-            changeAuditor = ChangeAuditor.posBigDecimal(1.refined).optional,
-            disabled = p.readonly
-          ).withMods(^.autoComplete.off),
-          FormLabel("signal-to-noise-at".refined)("at"),
-          FormInputTextView(
-            id = "signal-to-noise-at".refined,
-            groupClass = ExploreStyles.WarningInput.when_(signalToNoiseAt.get.isEmpty),
-            postAddons = signalToNoiseAt.get.fold(List(requiredForITC))(_ => Nil),
-            value = signalToNoiseAt,
+        ReactFragment(
+          FormInputTextView[View, Option[Wavelength]](
+            id = "configuration-wavelength".refined,
+            value = wv,
+            label = ReactFragment(
+              "Wavelength",
+              HelpIcon("configuration/wavelength.md".refined)
+            ),
+            groupClass = ExploreStyles.WarningInput.when_(wv.get.isEmpty),
+            postAddons = wv.get.fold(List(requiredForITC))(_ => Nil),
             units = "μm",
             validFormat = wvMicroInput,
-            changeAuditor = snAtWvChangeAuditor,
+            changeAuditor = wvChangeAuditor,
             disabled = p.readonly
-          ).clearable(^.autoComplete.off)
-        ),
-        FormInputTextView(
-          id = "wavelength-range".refined,
-          value = wavelengthDelta,
-          units = "μm",
-          label = ReactFragment(
-            "Δλ",
-            HelpIcon("configuration/wavelength_coverage.md".refined)
+          ).clearable(^.autoComplete.off),
+          FormInputTextView(
+            id = "configuration-resolution-power".refined,
+            value = resolution,
+            label = ReactFragment(
+              "λ / Δλ",
+              HelpIcon("configuration/spectral_resolution.md".refined)
+            ),
+            validFormat = InputValidSplitEpi.posInt.optional,
+            changeAuditor = ChangeAuditor.posInt.optional,
+            disabled = p.readonly
+          ).clearable(^.autoComplete.off),
+          FormLabel("signal-to-noise".refined)(
+            "S / N",
+            HelpIcon("configuration/signal_to_noise.md".refined)
           ),
-          validFormat = wvcMicroInput,
-          changeAuditor = wvChangeAuditor,
-          disabled = p.readonly
-        ).clearable(^.autoComplete.off),
-        FormLabel("focal-plane".refined)("Focal Plane",
-                                         HelpIcon("configuration/focal_plane.md".refined)
-        ),
-        <.div(
-          LucumaPrimeStyles.FormField |+| ExploreStyles.BasicConfigurationFocalPlane,
-          FormEnumDropdownOptionalView(
-            id = "focal-plane".refined,
-            placeholder = "Any",
-            value = focalPlane,
-            disabled = p.readonly
+          <.div(
+            LucumaPrimeStyles.FormField |+| ExploreStyles.BasicConfigurationSNAt,
+            FormInputTextView(
+              id = "signal-to-noise".refined,
+              value = signalToNoise,
+              groupClass = ExploreStyles.WarningInput.when_(signalToNoise.get.isEmpty),
+              validFormat = ExploreModelValidators.signalToNoiseValidSplitEpi.optional,
+              postAddons = signalToNoise.get.fold(List(requiredForITC))(_ => Nil),
+              changeAuditor = ChangeAuditor.posBigDecimal(1.refined).optional,
+              disabled = p.readonly
+            ).withMods(^.autoComplete.off),
+            FormLabel("signal-to-noise-at".refined)("at"),
+            FormInputTextView(
+              id = "signal-to-noise-at".refined,
+              groupClass = ExploreStyles.WarningInput.when_(signalToNoiseAt.get.isEmpty),
+              postAddons = signalToNoiseAt.get.fold(List(requiredForITC))(_ => Nil),
+              value = signalToNoiseAt,
+              units = "μm",
+              validFormat = wvMicroInput,
+              changeAuditor = snAtWvChangeAuditor,
+              disabled = p.readonly
+            ).clearable(^.autoComplete.off)
           ),
           FormInputTextView(
-            id = "focal-plane-angle".refined,
-            value = focalPlaneAngle,
-            units = "arcsec",
-            validFormat = slitLengthFormat,
-            changeAuditor = slitLengthChangeAuditor,
+            id = "wavelength-range".refined,
+            value = wavelengthDelta,
+            units = "μm",
+            label = ReactFragment(
+              "Δλ",
+              HelpIcon("configuration/wavelength_coverage.md".refined)
+            ),
+            validFormat = wvcMicroInput,
+            changeAuditor = wvChangeAuditor,
             disabled = p.readonly
-          ).clearable(^.autoComplete.off)
-        ),
-        FormEnumDropdownOptionalView(
-          id = "spectroscopy-capability".refined,
-          placeholder = "None",
-          value = spectroscopyCapability,
-          label = ReactFragment(
-            "Capability",
-            HelpIcon("configuration/capability.md".refined)
+          ).clearable(^.autoComplete.off),
+          FormLabel("focal-plane".refined)("Focal Plane",
+                                           HelpIcon("configuration/focal_plane.md".refined)
           ),
-          disabled = p.readonly
+          <.div(
+            LucumaPrimeStyles.FormField |+| ExploreStyles.BasicConfigurationFocalPlane,
+            FormEnumDropdownOptionalView(
+              id = "focal-plane".refined,
+              placeholder = "Any",
+              value = fpView,
+              disabled = true
+            ),
+            FormInputTextView(
+              id = "focal-plane-angle".refined,
+              value = focalPlaneAngle,
+              units = "arcsec",
+              validFormat = slitLengthFormat,
+              changeAuditor = slitLengthChangeAuditor,
+              disabled = p.readonly
+            ).clearable(^.autoComplete.off)
+          ),
+          FormEnumDropdownOptionalView(
+            id = "spectroscopy-capability".refined,
+            placeholder = "None",
+            value = spectroscopyCapability,
+            label = ReactFragment(
+              "Capability",
+              HelpIcon("configuration/capability.md".refined)
+            ),
+            disabled = true
+          )
         )
-      )
-    }
+      }
 }
