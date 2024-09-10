7d705028869516aa7d9876a692314b10d39b6d08
Carlos Quiroz <carlos.quiroz@noirlab.edu>
2024-09-10 14:15:21 -0300

    Fix promotion scripts

--- promoteStagingToProduction.sh
+++ promoteStagingToProduction.sh
@@ -3,6 +3,8 @@
 # WARNING: This will apply CURRENT LOCAL migrations to the production server,
 # which may not necessarily be in sync with the staging server.
 cd hasura/user-prefs
-hasura migrate apply --endpoint https://user-prefs.herokuapp.com 
+unset NODE_OPTIONS
+hasura migrate apply --endpoint https://user-prefs.herokuapp.com --database-name default
+hasura metadata apply --endpoint https://user-prefs.herokuapp.com
 cd ../..
 firebase hosting:clone explore-gemini-stage:live explore-gemini-prod:live
