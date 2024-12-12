b82cdfd085b3f2b55fa8478b2c17b48745072554
Raúl Piaggio <rpiaggio@gemini.edu>
2024-12-12 18:15:29 -0300

    more crystal hooks

--- explore/src/main/scala/explore/monadicHooks/crystal.scala
+++ explore/src/main/scala/explore/monadicHooks/crystal.scala
@@ -4,9 +4,11 @@
 package explore.monadicHooks
 
 import crystal.react.View
+import crystal.react.ReuseView
 import crystal.react.hooks.*
 import japgolly.scalajs.react.Reusable
 import japgolly.scalajs.react.util.DefaultEffects.Async as DefaultA
+import japgolly.scalajs.react.util.DefaultEffects.Sync as DefaultS
 
 /**
  * Provides a context in which to run a single effect at a time. When a new effect is submitted, the
@@ -17,14 +19,40 @@ import japgolly.scalajs.react.util.DefaultEffects.Async as DefaultA
 inline def useSingleEffect: HookResult[Reusable[UseSingleEffect[DefaultA]]] =
   UseSingleEffect.hook.toHookResult
 
+/** Creates component state that is reused while it's not updated. */
+inline def useSerialState[A](initialValue: => A): HookResult[UseSerialState[A]] =
+  UseSerialState.hook(initialValue).toHookResult
+
+/**
+ * Given a state, allows registering callbacks which are triggered when the state changes.
+ */
+inline def useStateCallback[A](
+  state: => Hooks.UseState[A]
+): HookResult[Reusable[(A => DefaultS[Unit]) => DefaultS[Unit]]] =
+  UseStateCallback.hook(state).toHookResult
+
 /** Creates component state as a View */
 inline def useStateView[A]: A => HookResult[View[A]] =
   UseStateView.hook.toHookResult
 
+/** Creates component state as a View */
+inline def useStateViewWithReuse[A: ClassTag: Reusability]: A => HookResult[ReuseView[A]] =
+  UseStateViewWithReuse.hook.toHookResult
+
+/** Creates component state as a View that is reused while it's not updated. */
+inline def useSerialStateView[A]: A => HookResult[ReuseView[A]] =
+  UseSerialStateView.hook.toHookResult
+
+// ... useAsyncEffect and friends
+
 // Custom hook example
 import cats.syntax.all.*
 import japgolly.scalajs.react.Callback
+import japgolly.scalajs.react.hooks.Hooks
+import scala.reflect.ClassTag
+import japgolly.scalajs.react.Reusability
 
+// TODO REMOVE, this is just PoC
 /**
  * Custom hook that keeps a counter and provides a function that adds to it and reports the new
  * value to the console.
