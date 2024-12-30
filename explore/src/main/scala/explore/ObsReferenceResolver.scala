061d10ebd1dfb3b66dc49b0a759e87c82d5e0f1e
Raúl Piaggio <rpiaggio@gemini.edu>
2024-12-30 19:27:16 -0300

    migrate single component

--- explore/src/main/scala/explore/ObsReferenceResolver.scala
+++ explore/src/main/scala/explore/ObsReferenceResolver.scala
@@ -15,36 +15,34 @@ import explore.utils.ToastCtx
 import japgolly.scalajs.react.*
 import japgolly.scalajs.react.vdom.html_<^.*
 import lucuma.core.model.ObservationReference
-import lucuma.react.common.ReactFnProps
+import lucuma.react.common.*
 import lucuma.react.primereact.Message.Severity
 import lucuma.ui.syntax.pot.*
 import queries.common.ObsQueriesGQL.ResolveObsReference
 
 case class ObsReferenceResolver(obsRef: ObservationReference)
-    extends ReactFnProps(ObsReferenceResolver.component)
+    extends ReactFnProps(ObsReferenceResolver)
 
-object ObsReferenceResolver:
-  private type Props = ObsReferenceResolver
+object ObsReferenceResolver
+    extends ReactFnComponent[ObsReferenceResolver](props =>
+      for
+        ctx    <- useContext(AppContext.ctx)
+        result <- useEffectResultOnMount:
+                    import ctx.given
 
-  private val component =
-    ScalaFnComponent
-      .withHooks[Props]
-      .useContext(AppContext.ctx)
-      .useEffectResultOnMountBy: (props, ctx) =>
-        import ctx.given
-
-        ResolveObsReference[IO]
-          .query(props.obsRef.assign)
-          .flatMap: data =>
-            data.observation
-              .map: o =>
-                ctx
-                  .pushPage((AppTab.Observations, o.program.id, Focused.singleObs(o.id)).some)
-                  .to[IO]
-              .getOrElse:
-                ToastCtx[IO].showToast(
-                  s"Observation reference ${props.obsRef.label} does not exist.",
-                  Severity.Error
-                ) >> ctx.pushPage(none).to[IO]
-      .render: (props, _, result) =>
-        result.renderPot(_ => EmptyVdom)
+                    ResolveObsReference[IO]
+                      .query(props.obsRef.assign)
+                      .flatMap: data =>
+                        data.observation
+                          .map: o =>
+                            ctx
+                              .pushPage:
+                                (AppTab.Observations, o.program.id, Focused.singleObs(o.id)).some
+                              .to[IO]
+                          .getOrElse:
+                            ToastCtx[IO].showToast(
+                              s"Observation reference ${props.obsRef.label} does not exist.",
+                              Severity.Error
+                            ) >> ctx.pushPage(none).to[IO]
+      yield result.renderPot(_ => EmptyVdom)
+    )
