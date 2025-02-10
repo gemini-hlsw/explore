03d928d6c841570aace0069c23ff624dd4e23ec2
Todd Burnside <tburnside@gemini.edu>
2025-02-10 12:09:43 -0800

    Review comments, switch to ReactFnComponent

--- explore/src/main/scala/explore/programs/ProgramDetailsTile.scala
+++ explore/src/main/scala/explore/programs/ProgramDetailsTile.scala
@@ -20,102 +20,101 @@ import japgolly.scalajs.react.vdom.html_<^.*
 import lucuma.core.model.Program
 import lucuma.core.syntax.display.*
 import lucuma.core.util.DateInterval
+import lucuma.react.common.ReactFnComponent
 import lucuma.react.common.ReactFnProps
 import lucuma.refined.*
 import lucuma.ui.primereact.CheckboxView
 import lucuma.ui.primereact.FormInfo
 import lucuma.ui.primereact.given
 
-import java.time.LocalDate
-
 case class ProgramDetailsTile(
   programId:         Program.Id,
   programDetails:    View[ProgramDetails],
   programTimes:      Pot[ProgramTimes],
   userIsReadonlyCoi: Boolean
-) extends ReactFnProps(ProgramDetailsTile.component)
+) extends ReactFnProps(ProgramDetailsTile)
 
-object ProgramDetailsTile:
-  private type Props = ProgramDetailsTile
+object ProgramDetailsTile
+    extends ReactFnComponent[ProgramDetailsTile](props =>
+      useContext(AppContext.ctx).map: ctx =>
+        import ctx.given
 
-  private val component = ScalaFnComponent[Props]: props =>
-    useContext(AppContext.ctx).map: ctx =>
-      import ctx.given
+        val details: ProgramDetails        = props.programDetails.get
+        val thesis: Boolean                = details.allUsers.exists(_.thesis.exists(_ === true))
+        val users: View[List[ProgramUser]] = props.programDetails.zoom(ProgramDetails.allUsers)
+        val newDataNotificationView        =
+          props.programDetails
+            .zoom(ProgramDetails.shouldNotify)
+            .withOnMod(b => ProgramQueries.updateGoaShouldNotify[IO](props.programId, b).runAsync)
 
-      val details: ProgramDetails               = props.programDetails.get
-      val thesis: Boolean                       = details.allUsers.exists(_.thesis.exists(_ === true))
-      val users: View[List[ProgramUser]]        = props.programDetails.zoom(ProgramDetails.allUsers)
-      val newDataNotificationView               =
-        props.programDetails
-          .zoom(ProgramDetails.shouldNotify)
-          .withOnMod(b => ProgramQueries.updateGoaShouldNotify[IO](props.programId, b).runAsync)
-      val cfpActivePeriod: Option[DateInterval] = details.proposal.flatMap(_.call).map(_.active)
+        // Since the program tab is for Accepted proposals only, we should always have a CfP.
+        val cfpActivePeriod: Option[DateInterval] = details.proposal.flatMap(_.call).map(_.active)
 
-      // We `should` always have a call for proposal if we get here, but...
-      def dateOrMissing(o: Option[LocalDate], label: String) =
-        val s = o.fold("Missing!")(Constants.GppDateFormatter.format)
-        FormInfo(s, label)
-
-      <.div(ExploreStyles.ProgramDetailsTile)(
-        <.div(ExploreStyles.ProgramDetailsInfoArea, ExploreStyles.ProgramDetailsLeft)(
-          FormInfo(details.reference.map(_.label).getOrElse("---"), "Reference"),
-          dateOrMissing(cfpActivePeriod.map(_.start), "Start"),
-          dateOrMissing(cfpActivePeriod.map(_.end), "End"),
-          // Thesis should be set True if any of the investigators will use the proposal as part of their thesis (3390)
-          FormInfo(if (thesis) "Yes" else "No", "Thesis"),
-          FormInfo(s"${details.proprietaryMonths} months", "Proprietary")
-        ),
-        <.div(
-          TimeAwardTable(details.allocations),
-          TimeAccountingTable(props.programTimes)
-        ),
-        <.div(ExploreStyles.ProgramDetailsInfoArea)(
-          SupportUsers(
-            props.programId,
-            users,
-            "Principal Support",
-            SupportUsers.SupportRole.Primary
+        <.div(ExploreStyles.ProgramDetailsTile)(
+          <.div(ExploreStyles.ProgramDetailsInfoArea, ExploreStyles.ProgramDetailsLeft)(
+            FormInfo(details.reference.map(_.label).getOrElse("---"), "Reference"),
+            cfpActivePeriod.map(di =>
+              React.Fragment(
+                FormInfo(Constants.GppDateFormatter.format(di.start), "Start"),
+                FormInfo(Constants.GppDateFormatter.format(di.end), "End")
+              )
+            ),
+            // Thesis should be set True if any of the investigators will use the proposal as part of their thesis (3390)
+            FormInfo(if (thesis) "Yes" else "No", "Thesis"),
+            FormInfo(s"${details.proprietaryMonths} months", "Proprietary")
           ),
-          SupportUsers(
-            props.programId,
-            users,
-            "Additional Support",
-            SupportUsers.SupportRole.Secondary
-          ),
-
-          // The two Notifications flags are user-settable and determine whether the archive sends email notifications for new data and whether the ODB sends notifications for expired timing windows (3388, 3389)
           <.div(
-            ExploreStyles.ProgramDetailsRight,
-            FormInfo(
-              CheckboxView(
-                id = "shouldNotify".refined,
-                value = newDataNotificationView,
-                label = "New Science Data",
-                disabled = props.userIsReadonlyCoi
-              ),
-              "Notifications"
+            TimeAwardTable(details.allocations),
+            TimeAccountingTable(props.programTimes)
+          ),
+          <.div(ExploreStyles.ProgramDetailsInfoArea)(
+            SupportUsers(
+              props.programId,
+              users,
+              "Principal Support",
+              SupportUsers.SupportRole.Primary
+            ),
+            SupportUsers(
+              props.programId,
+              users,
+              "Additional Support",
+              SupportUsers.SupportRole.Secondary
+            ),
+
+            // The two Notifications flags are user-settable and determine whether the archive sends email notifications for new data and whether the ODB sends notifications for expired timing windows (3388, 3389)
+            <.div(
+              ExploreStyles.ProgramDetailsRight,
+              FormInfo(
+                CheckboxView(
+                  id = "shouldNotify".refined,
+                  value = newDataNotificationView,
+                  label = "New Science Data",
+                  disabled = props.userIsReadonlyCoi
+                ),
+                "Notifications"
+              )
+              // FormInfo(
+              //   CheckboxView(
+              //     id = "expiredTimingWindows".refined,
+              //     value = ???,
+              //     label = "Expired Timing Windows"
+              //   ),
+              //   ""
+              // )
             )
-            // FormInfo(
-            //   CheckboxView(
-            //     id = "expiredTimingWindows".refined,
-            //     value = ???,
-            //     label = "Expired Timing Windows"
-            //   ),
-            //   ""
+
+            // The Eavesdropping` UI will allow PIs of accepted programs to select dates when they are available for eavesdropping. This is not needed for XT. (NEED TICKET)
+            // <.div(
+            //   ExploreStyles.ProgramDetailsRight,
+            //   FormInfo(
+            //     CheckboxView(
+            //       id = "eavesdropping".refined,
+            //       value = ???,
+            //       label = ??? // instead of a label there will be a date picker or something?
+            //     ),
+            //     "Eavesdropping"
+            //   )
             // )
           )
-
-          // The Eavesdropping` UI will allow PIs of accepted programs to select dates when they are available for eavesdropping. This is not needed for XT. (NEED TICKET)
-          // <.div(
-          //   ExploreStyles.ProgramDetailsRight,
-          //   FormInfo(
-          //     CheckboxView(
-          //       id = "eavesdropping".refined,
-          //       value = ???,
-          //       label = ??? // instead of a label there will be a date picker or something?
-          //     ),
-          //     "Eavesdropping"
-          //   )
-          // )
         )
-      )
+    )
