b59d1d96566078160e129912711c012bc9884ab1
Carlos Quiroz <carlos.quiroz@noirlab.edu>
2025-03-17 16:46:40 -0300

    Update endpoints

--- promoteStagingAllToProduction.sh
+++ promoteStagingAllToProduction.sh
@@ -4,4 +4,4 @@ pushd .
 source ./promote.sh staging production
 
 popd
-echo "firebase hosting:clone explore-gemini-stage:live explore-gemini-prod:live"
+firebase hosting:clone explore-gemini-stage:live explore-gemini-prod:live
