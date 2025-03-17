7c16320c5ab1987a24c4d9bf79f7308d2ca7a6bb
Raúl Piaggio <rpiaggio@gemini.edu>
2025-03-17 09:26:39 -0300

    versions

--- project/Versions.scala
+++ project/Versions.scala
@@ -29,7 +29,7 @@ object Versions {
   val lucumaSchemas          = "0.123.0"
   val lucumaOdbSchema        = "0.18.6"
   val lucumaSSO              = "0.8.5"
-  val lucumaUI               = "0.133.0-6-3324965-20250317T120432Z-SNAPSHOT"
+  val lucumaUI               = "0.133.2"
   val monocle                = "3.3.0"
   val mouse                  = "1.3.2"
   val mUnit                  = "1.1.0"
