1587e03d19ca9fb4473f8aa3220dc829e7baa749
Raúl Piaggio <rpiaggio@gemini.edu>
2025-04-09 19:00:54 -0300

    bump schemas

--- project/Versions.scala
+++ project/Versions.scala
@@ -25,7 +25,7 @@ object Versions {
   val lucumaITC              = "0.34.0"
   val lucumaReact            = "0.82.0"
   val lucumaRefined          = "0.1.4"
-  val lucumaSchemas          = "0.128.0"
+  val lucumaSchemas          = "0.130.0"
   val lucumaOdbSchema        = "0.19.3"
   val lucumaSSO              = "0.8.9"
   val lucumaUI               = "0.137.0"
