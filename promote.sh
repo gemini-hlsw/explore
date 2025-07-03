fd961424139460587bb6f1beeb3fbc1dc2bf0c33
Raúl Piaggio <rpiaggio@gemini.edu>
2025-07-03 11:59:32 -0300

    ensure container login for itc promotion

--- promote.sh
+++ promote.sh
@@ -15,23 +15,29 @@ fi
 
 echo "##### Capturing a db backup on $TARGET_ENV."
 heroku pg:backups:capture --app lucuma-postgres-odb-${TARGET_ENV}
+echo
 
 echo "##### Promoting all services from $SOURCE_ENV to $TARGET_ENV"
+echo
 
 # Promote SSO
-echo "Promote SSO to $TARGET_ENV"
+echo "## Promote SSO to $TARGET_ENV"
 heroku pipelines:promote -a lucuma-sso-${SOURCE_ENV} -t lucuma-sso-${TARGET_ENV}
+echo
 
 # Promote ITC
-echo "Promote ITC to $TARGET_ENV"
+echo "## Promote ITC to $TARGET_ENV"
+heroku container:login
 docker pull registry.heroku.com/itc-${SOURCE_ENV}/web:latest
 docker tag registry.heroku.com/itc-${SOURCE_ENV}/web:latest registry.heroku.com/itc-${TARGET_ENV}/web:latest
 docker push registry.heroku.com/itc-${TARGET_ENV}/web:latest
 heroku container:release web --app itc-${TARGET_ENV}
+echo
 
 # Promote ODB
-echo "Promote ODB to $TARGET_ENV"
+echo "## Promote ODB to $TARGET_ENV"
 heroku pipelines:promote -a lucuma-postgres-odb-${SOURCE_ENV} -t lucuma-postgres-odb-${TARGET_ENV}
+echo
 
 # Update user preferences
 HASURA_ENDPOINT=""
@@ -47,7 +53,8 @@ case $TARGET_ENV in
     exit 1
     ;;
 esac
-echo "Promote user preferences db to $TARGET_ENV"
+
+echo "## Promote user preferences db to $TARGET_ENV"
 cd hasura/user-prefs
 unset NODE_OPTIONS
 hasura migrate apply --endpoint "$HASURA_ENDPOINT" --database-name default
