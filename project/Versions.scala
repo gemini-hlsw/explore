28cbb82a306b80f54ef6a5966ad9e223f264757a
Carlos Quiroz <carlos.quiroz@noirlab.edu>
2025-08-11 17:08:43 -0400

    Update schema

--- project/Versions.scala
+++ project/Versions.scala
@@ -27,7 +27,7 @@ object Versions {
   val lucumaRefined          = "0.1.4"
   val lucumaOdbSchema        = "0.26.3"
   val lucumaSSO              = "0.9.6"
-  val lucumaUI               = "0.153.1"
+  val lucumaUI               = "0.154.0"
   val monocle                = "3.3.0"
   val mouse                  = "1.3.2"
   val mUnit                  = "1.1.1"
